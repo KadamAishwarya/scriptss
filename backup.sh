147de596701fb25292b265bdbbc778f7306a1ff3
Ubuntu <ubuntu@ip-172-31-28-171.ec2.internal>
2026-02-03 03:27:25 +0000

    added shell scripts

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source_dir="/home/ubuntu/scripts"
+destination_dir="/home/ubuntu/backup"
+
+function create_backup {
+
+	timestamp=$(date '+%Y-%m-%d-%H-%M-%S' )
+
+        backup_dir="${destination_dir}/backup_${timestamp}"
+
+        zip -r "${backup_dir}.zip" "$source_dir"
+
+        echo "Backup Completed"
+}
+
+create_backup $source_dir $destination_dir
---
+++ monitor_process.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+<<help
+This script takes a process/service as an argument
+and checks/monitors its status
+./monitori_process.sh <process_name>.
+help
+
+
+read -p "What do you wish to do with your service $1: (status/pid)" choice
+
+function check_status()  {
+
+             echo "Monitoring service status"
+             sudo systemctl status $1
+
+}
+
+if [[  $choice == "status"    ]];
+then 
+	check_status $3
+elif [[  $choice == "pid"    ]];
+then
+      echo "process id of $1 $( pgrep $1 )"
+
+else
+    echo "wrong choice"
+
+fi    
